19ef67619fd8366803a2a6f6d63ff2ee8acfa794
tjlastnumber <www.514163307@qq.com>
2018-09-25 15:02:21 +0800

    使用vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import '@babel/polyfill'
 import Vue from 'vue'
+import Vuex from 'vuex'
 import './plugins/vuetify'
 import App from './App.vue'
 
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
 new Vue({
