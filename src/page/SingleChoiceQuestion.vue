f7ca880a059172087d2aea816699cbb0951b2178
tjlastnumber <www.514163307@qq.com>
2018-09-26 11:52:38 +0800

    update

--- src/page/SingleChoiceQuestion.vue
+++ src/page/SingleChoiceQuestion.vue
@@ -12,14 +12,21 @@
                 flat
                 solo
                 prepend-icon="search"
-                placeholder="Type something"
+                placeholder="题目"
                 v-model="search"
                 hide-details
                 class="hidden-sm-and-down"
               ></v-text-field>
-              <v-btn icon>
-                <v-icon>filter_list</v-icon>
-              </v-btn>
+              <v-flex xs2>
+                <v-select
+                  :items="['全部', '已发布', '未发布']"
+                  v-model="state"
+                  menu-props="auto"
+                  hide-details
+                  label="发布状态"
+                  single-line
+                ></v-select>
+              </v-flex>
             </v-toolbar>
             <v-divider></v-divider>
             <v-card-text class="pa-0">
@@ -27,25 +34,23 @@
                 :headers="complex.headers"
                 :search="search"
                 :items="complex.items"
-                :rows-per-page-items="[10,25,50,{text:'All','value':-1}]"
-                class="elevation-1"
+                :rows-per-page-items="[5,10,25,50,{text:'All','value':-1}]"
+                :pagination.sync="pagination"
+                :total-items="total"
+                :loading="loading"
                 item-key="name"
-                select-all
+                no-data-text="无"
+                rows-per-page-text="每页显示数量："
                 v-model="complex.selected"
               >
                 <template slot="items" slot-scope="props">
-                  <td>
-                    <v-checkbox primary hide-details v-model="props.selected"></v-checkbox>
-                  </td>
-                  <td>
-                    <v-avatar size="32">
-                      <img :src="props.item.avatar" alt="">
-                    </v-avatar>
-                  </td>
                   <td>{{ props.item.name }}</td>
-                  <td>{{ props.item.email }}</td>
-                  <td>{{ props.item.phone }}</td>
-                  <td>
+                  <td class="text-xs-right">{{ props.item.calories }}</td>
+                  <td class="text-xs-right">{{ props.item.fat }}</td>
+                  <td class="text-xs-right">{{ props.item.carbs }}</td>
+                  <td class="text-xs-right">{{ props.item.protein }}</td>
+                  <td class="text-xs-right">{{ props.item.iron }}</td>
+                  <td class="text-xs-right">
                     <v-btn
                       depressed
                       outline
@@ -54,6 +59,7 @@
                       dark
                       color="primary"
                       small
+                      @click.native="edit"
                     >
                       <v-icon>edit</v-icon>
                     </v-btn>
@@ -65,12 +71,16 @@
                       dark
                       color="pink"
                       small
+                      @click.native="deleteItem"
                     >
                       <v-icon>delete</v-icon>
                     </v-btn>
                   </td>
                 </template>
               </v-data-table>
+              <div class="text-xs-center pb-3">
+                <v-pagination v-model="pagination.page" :length="pages"></v-pagination>
+              </div>
             </v-card-text>
           </v-card>
         </v-flex>
@@ -83,32 +93,143 @@
 export default {
   data() {
     return {
-      search: '',
+      search: "",
+      state: '全部',
       complex: {
-        selected: [],
         headers: [{
-            text: 'Avatar',
-            value: 'avatar'
-          },
-          {
-            text: 'Name',
+            text: 'Dessert (100g serving)',
+            align: 'left',
+            sortable: false,
             value: 'name'
           },
           {
-            text: 'Email',
-            value: 'email'
+            text: 'Calories',
+            value: 'calories'
           },
           {
-            text: 'Phone',
-            value: 'phone'
+            text: 'Fat (g)',
+            value: 'fat'
           },
           {
-            text: 'Action',
+            text: 'Carbs (g)',
+            value: 'carbs'
+          },
+          {
+            text: 'Protein (g)',
+            value: 'protein'
+          },
+          {
+            text: 'Iron (%)',
+            value: 'iron'
+          }, {
+            text: 'Active',
             value: ''
-          },
+          }
         ],
-        items: []
-      }
+        items: [{
+            value: false,
+            name: 'Frozen Yogurt',
+            calories: 159,
+            fat: 6.0,
+            carbs: 24,
+            protein: 4.0,
+            iron: '1%'
+          },
+          {
+            value: false,
+            name: 'Ice cream sandwich',
+            calories: 237,
+            fat: 9.0,
+            carbs: 37,
+            protein: 4.3,
+            iron: '1%'
+          },
+          {
+            value: false,
+            name: 'Eclair',
+            calories: 262,
+            fat: 16.0,
+            carbs: 23,
+            protein: 6.0,
+            iron: '7%'
+          },
+          {
+            value: false,
+            name: 'Cupcake',
+            calories: 305,
+            fat: 3.7,
+            carbs: 67,
+            protein: 4.3,
+            iron: '8%'
+          },
+          {
+            value: false,
+            name: 'Gingerbread',
+            calories: 356,
+            fat: 16.0,
+            carbs: 49,
+            protein: 3.9,
+            iron: '16%'
+          },
+          {
+            value: false,
+            name: 'Jelly bean',
+            calories: 375,
+            fat: 0.0,
+            carbs: 94,
+            protein: 0.0,
+            iron: '0%'
+          },
+          {
+            value: false,
+            name: 'Lollipop',
+            calories: 392,
+            fat: 0.2,
+            carbs: 98,
+            protein: 0,
+            iron: '2%'
+          },
+          {
+            value: false,
+            name: 'Honeycomb',
+            calories: 408,
+            fat: 3.2,
+            carbs: 87,
+            protein: 6.5,
+            iron: '45%'
+          },
+          {
+            value: false,
+            name: 'Donut',
+            calories: 452,
+            fat: 25.0,
+            carbs: 51,
+            protein: 4.9,
+            iron: '22%'
+          },
+          {
+            value: false,
+            name: 'KitKat',
+            calories: 518,
+            fat: 26.0,
+            carbs: 65,
+            protein: 7,
+            iron: '6%'
+          }
+        ]
+      },
+      pagination: {},
+      total: 0,
+      loading: true
+    }
+  },
+  computed: {
+    pages() {
+      if (this.pagination.rowsPerPage == null ||
+        this.pagination.totalItems == null
+      ) return 0
+
+      return Math.ceil(this.pagination.totalItems / this.pagination.rowsPerPage)
     }
   }
 }
